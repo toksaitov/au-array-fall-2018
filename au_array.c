24c16ae660b4cbee55e417138055cb9978041dcd
derkenbaev <derkenbaev.azamat@gmail.com>
2018-12-14 15:53:45 +0600

    add implementation of the au_array_get method

--- au_array.c
+++ au_array.c
@@ -78,6 +78,7 @@ void *au_array_last(au_array *array)
 
 void *au_array_get(au_array *array, size_t index)
 {
-    return NULL;
+	if (array->elements == NULL || index >= array->length) return NULL;
+    return array->elements + index * (array->element_size);
 }
 
