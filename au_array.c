daf453e0e6f1d77ee8a2f017c32df135656925aa
derkenbaev <derkenbaev.azamat@gmail.com>
2018-12-14 16:19:12 +0600

    add implementation of the test_au_array_last unit test method

--- au_array.c
+++ au_array.c
@@ -74,7 +74,7 @@ void *au_array_first(au_array *array)
 void *au_array_last(au_array *array)
 {
 	if (array->elements == NULL) return NULL;
-    return array->elements + (array->length - 1);
+    return array->elements + (array->length - 1) * array->element_size;
 }
 
 void *au_array_get(au_array *array, size_t index)
