30020407607c7835d9a92884075f3c61696e66f1
derkenbaev <derkenbaev.azamat@gmail.com>
2018-12-18 01:11:53 +0600

    replace tab with spaces uwu

--- au_array.c
+++ au_array.c
@@ -73,13 +73,12 @@ void *au_array_first(au_array *array)
 
 void *au_array_last(au_array *array)
 {
-	if (array->elements == NULL) return NULL;
+    if (array->elements == NULL) return NULL;
     return array->elements + (array->length - 1) * array->element_size;
 }
 
 void *au_array_get(au_array *array, size_t index)
 {
-	if (array->elements == NULL || index >= array->length) return NULL;
+    if (array->elements == NULL || index >= array->length) return NULL;
     return array->elements + index * (array->element_size);
 }
-
