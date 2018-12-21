0ed7d0e9284783d80af09ad011b032698199a3ab
Erik Duisheev <erik.duysheev@mail.ru>
2018-12-21 10:16:22 +0600

    implement au_array_free_with_elements method itself

--- au_array.c
+++ au_array.c
@@ -43,7 +43,9 @@ void au_array_free(au_array *array)
 
 void au_array_free_with_elements(au_array *array)
 {
-
+    for (int i = 0; i < au_array_length(array); i++) {
+        free(array[i]);
+    }
 }
 
 void au_array_free_with_element_handler(au_array *array, au_array_element_handler handler)
@@ -79,6 +81,7 @@ void *au_array_first(au_array *array)
 {
     return NULL;
 }
+
 
 void *au_array_last(au_array *array)
 {
