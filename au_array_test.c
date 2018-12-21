3acdab1ca1c7d88e60f13fb5ca6b7266fd735de4
mdawlatshah <adawlatshah@gmail.com>
2018-12-21 12:44:27 +0600

    test array size implementation

--- au_array_test.c
+++ au_array_test.c
@@ -70,7 +70,8 @@ void test_au_array_length()
 
 void test_au_array_size()
 {
-    assert(false);
+	au_array *array = au_array_create_of_length(1, sizeof(char));
+	assert(au_array_size(array) == sizeof(char));
 }
 
 void test_au_array_element_size()
